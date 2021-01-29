4dc5ec8ebae53d3a24f5effdb73ee93d10465ea1
Ernie <40248055+Ernie1812@users.noreply.github.com>
2021-01-29 09:58:51 +0000

    working country borders

--- assets/php/openCage.php
+++ assets/php/openCage.php
@@ -2,8 +2,8 @@
 
 	$executionStartTime = microtime(true) / 1000;
     //$url= 'https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=d15534ffc3514c07817111eacd8ea75b';
-    $url = 'https://api.opencagedata.com/geocode/v1/json?q='. $_REQUEST['lat'] .'+' . $_REQUEST['lng'] . '&key=d15534ffc3514c07817111eacd8ea75b';
-
+    //$url = 'https://api.opencagedata.com/geocode/v1/json?q='. $_REQUEST['lat'] .'+' . $_REQUEST['lng'] . '&key=d15534ffc3514c07817111eacd8ea75b';
+	$url='http://api.ipstack.com/check?access_key=a0bc50db7a7bfab522a89f9b5241d2be';
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -19,7 +19,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "mission saved";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode['results'];
+	$output['data']['userIpInfo'] = $decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
