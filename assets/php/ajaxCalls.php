f65a77ed2e70861065d8f383513d941bb06ce774
Ernie <40248055+Ernie1812@users.noreply.github.com>
2021-02-12 12:14:56 +0000

    added code to remove special characters, moved all  (except looped currency code) to end of php

--- assets/php/ajaxCalls.php
+++ assets/php/ajaxCalls.php
@@ -10,13 +10,16 @@
     $capitalLat = null;
     $capitalLng = null;
     $capitalCityNoSpace = null;
-
+    $capitalCityWiped = null;
     $currentCurrency = null;
     $wikiCountryName = null;
 
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
     $executionStartTime = microtime(true);
+
+    $unwanted_array = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+    'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c','è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i','ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o','ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'Ğ'=>'G', 'İ'=>'I', 'Ş'=>'S', 'ğ'=>'g', 'ı'=>'i', 'ş'=>'s', 'ü'=>'u', 'ă'=>'a', 'Ă'=>'A', 'ș'=>'s', 'Ș'=>'S', 'ț'=>'t', 'Ț'=>'T' );
 
     // get country border feature
     $countryBorders = json_decode(file_get_contents("../data/countryBorders.geo.json"), true);
@@ -28,7 +31,6 @@
             }
         }
 
-    $output['data']['border'] = $border;
     $countryName = $border['properties']['name'];
     $countryNameNoSpace = preg_replace('/\s+/', '%20', $countryName);
     $wikiCountryName = preg_replace('/\s+/', '_', $countryName);
@@ -50,15 +52,16 @@
     curl_close($ch);
 
     $restCountries = json_decode($result,true); 
-    $output['data']['restCountries'] = $restCountries;
+    
     $countryFullName = $restCountries['name'];
     $capitalCity = $restCountries['capital']; 
     $capitalCityNoSpace =  preg_replace('/\s+/', '%20', $capitalCity);
+    $capitalCityWiped = strtr( $capitalCityNoSpace, $unwanted_array );
     $currentCurrency = $restCountries['currencies'][0]['code'];
         
     
     //PositionStack API Call for capital city long and lat
-    $url ='http://api.positionstack.com/v1/forward?access_key=cc4a38f03554215037c505edf96abf81&query='. $capitalCityNoSpace .','.$countryNameNoSpace;
+    $url ='http://api.positionstack.com/v1/forward?access_key=cc4a38f03554215037c505edf96abf81&query='. $capitalCityWiped .','.$countryNameNoSpace;
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -70,7 +73,7 @@
     curl_close($ch);
     
     $capitalData = json_decode($result,true);   
-    $output['data']['capitalData'] = $capitalData['data'][0];
+    
     $capitalLat = $capitalData['data'][0]['latitude'];
     $capitalLng = $capitalData['data'][0]['longitude'];
     
@@ -335,7 +338,11 @@ foreach ($largeCities['records'] as $key => $value) {
     $output['status']['name'] = "ok";
     $output['status']['description'] = "success";
     $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-
+    
+    
+    $output['data']['border'] = $border;
+    $output['data']['restCountries'] = $restCountries;
+    $output['data']['capitalData'] = $capitalData['data'][0];
     $output['data']['weather'] = $weather;
     $output['data']['covidData'] = $covid;
     $output['data']['exchangeRates'] = $exchangeRates;
